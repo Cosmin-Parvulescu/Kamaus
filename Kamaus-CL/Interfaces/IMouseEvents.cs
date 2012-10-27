1e68257d96c2a5c5000963cb9c7c1b497e0ba005
unknown <Cosmin.Parvulescu@gmail.com>
2012-10-27 19:15:12 +0300

    Am adugat interfata comuna pt. mouse si o clasa simpla de mock

--- Kamaus-CL/Interfaces/IMouseEvents.cs
+++ Kamaus-CL/Interfaces/IMouseEvents.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Kamaus_CL.Interfaces
 {
-    interface IMouseEvents
+    public interface IMouseEvents
     {
+        event EventHandler MouseMove;
+        event EventHandler MouseLeftClick;
+        event EventHandler MouseRightClick;
+        event EventHandler MouseDoubleClick;
 
+        protected virtual void OnMouseMove(MouseMoveEventArgs e);
+        protected virtual void OnMouseLeftClick(MouseEventArgs e);
+        protected virtual void OnMouseRightClick(MouseEventArgs e);
+        protected virtual void OnMouseDoubleClick(MouseEventArgs e);
+    }
+
+    public class MouseMoveEventArgs : EventArgs
+    {
+        public int lastX, lastY;
+        public int newX, newY;
+    }
+
+    public class MouseEventArgs : EventArgs
+    {
+        public int x, y;
     }
 }
---
+++ Kamaus-CL/MouseMockup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamaus_CL
+{
+    public class MouseMockup : Interfaces.IMouseEvents
+    {
+        #region Interface Implementation
+        public event EventHandler MouseMove;
+        public event EventHandler MouseLeftClick;
+        public event EventHandler MouseRightClick;
+        public event EventHandler MouseDoubleClick;
+
+        public void OnMouseMove(Interfaces.MouseMoveEventArgs e)
+        {
+            if (MouseMove != null)
+            {
+                MouseMove(this, e);
+            }
+        }
+        public void OnMouseLeftClick(Interfaces.MouseEventArgs e)
+        {
+            if (MouseLeftClick != null)
+            {
+                MouseLeftClick(this, e);
+            }
+        }
+        public void OnMouseRightClick(Interfaces.MouseEventArgs e)
+        {
+            if (MouseRightClick != null)
+            {
+                MouseRightClick(this, e);
+            }
+        }
+        public void OnMouseDoubleClick(Interfaces.MouseEventArgs e)
+        {
+            if (MouseDoubleClick != null)
+            {
+                MouseDoubleClick(this, e);
+            }
+        }
+        #endregion
+
+        Random rand = new Random();
+
+        public MouseMockup() { }
+        public void DoubleClick()
+        {
+            OnMouseDoubleClick(new Interfaces.MouseEventArgs()
+            {
+                x = rand.Next(500),
+                y = rand.Next(500)
+            });
+        }
+        public void LeftClick()
+        {
+            OnMouseLeftClick(new Interfaces.MouseEventArgs()
+            {
+                x = rand.Next(500),
+                y = rand.Next(500)
+            });
+        }
+        public void RightClick()
+        {
+            OnMouseRightClick(new Interfaces.MouseEventArgs()
+            {
+                x = rand.Next(500),
+                y = rand.Next(500)
+            });
+        }
+        public void MouseMove()
+        {
+            OnMouseMove(new Interfaces.MouseMoveEventArgs()
+            {
+                lastX = rand.Next(500),
+                lastY = rand.Next(500),
+                newX = rand.Next(500),
+                newY = rand.Next(500)
+            });
+        }
+    }
+}
